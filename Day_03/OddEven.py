ccc25bfe865ae2ca644b0cf7f44b58065928c2b5
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-01-26 17:18:21 +0200

    Odd Even Checker

---
+++ Day_03/OddEven.py
@@ -0,0 +1,6 @@
+print("-------------- Check odd or Even -----------------")
+num = int(input("please enter a number\n"))
+if num % 2 == 0:
+    print("The number is even \n")
+else:
+    print("the number is odd ")
