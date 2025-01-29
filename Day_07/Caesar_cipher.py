c0e6be805edb311f79a684c22848a165becdc406
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-01-29 19:19:50 +0200

    caesar cipher

---
+++ Day_07/Caesar_cipher.py
@@ -0,0 +1,69 @@
+
+print('''⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣿⣿⢿⡶⠆⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⡿⠻⠋⣠⠀⢀⣶⠇⢠⣾⡿⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣼⠟⠋⠻⢁⣴⠀⣾⣿⠀⠾⠟⠀⠈⣉⣠⣦⡤⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠸⠃⣠⡆⠀⣿⡟⠀⠛⠃⠀⠀⣶⣶⣦⣄⠉⢁⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣰⡀⢰⣿⠇⠀⢉⣀⣀⠛⠿⠿⠦⠀⢀⣠⣤⣴⣾⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣿⠃⠀⠠⣴⣦⡈⠙⠛⠓⠀⢰⣶⣶⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀
+⠀⠀⢀⣤⠦⡀⠰⢷⣦⠈⠉⠉⠀⣰⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀
+⠀⠀⠈⠁⠀⠘⣶⣤⣄⣀⣨⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠃⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣯⡈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢨⣿⣿⣿⣷⣤⣈⡉⠛⠛⠛⠛⠻⠟⠛⠛⠛⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠙⠻⠿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀''')
+print("================== Hello iam Caesar Cipher i will help you to encrypt and decrypt your message ==================")
+
+
+# the encryption function
+cipher = [""]
+def encrypt(original_text, shift): 
+    for i in original_text:
+        encoded_text = chr((ord(i)) + shift)       # ord to get the ascii for the letter and chr to convert it to letter
+        cipher.append(encoded_text)
+        final_cipher = ''.join(cipher)
+    print(f"Your secret text is:  {final_cipher}\n")
+
+# the decryption function
+text = [""]
+def decrypt(cipher_text, shift):
+    for i in cipher_text:
+        original_text = chr((ord(i)) - shift)
+        text.append(original_text)
+        final_text = ''.join(text)
+    print(f"The original text is:  {final_text}\n")
+
+
+not_end = True
+while not_end:
+    x = input("Do you want to decrypt or encrypt: ")
+    if x == "decrypt":
+        decrypt(input("Enter the cipher text to decrypt\n"), int(input("Enter the shift factor: \n")))
+        y = input("do you want to decrypt or encrypt another message: y or n: ")
+        if y == "y":
+            text.clear()
+            continue
+            not_end = False
+        elif y == "n":
+            print("I was happy to help you to decrypt your message")
+            not_end = False
+        else:
+            print("Error, Please enter 'y' for yes and 'n' for no: ")
+
+    elif x == "encrypt":
+        encrypt(input("enter the text: "), int(input("enter the shift factor: ")))
+        z = input("do you want to decrypt or encrypt another message: y or n: ")
+        if z == "y":
+            cipher.clear()
+            continue
+            not_end = False
+        elif z == "n":
+            print("I was happy to help you to encrypt your message")
+            not_end = False
+        else:
+            print("Error, Please enter 'y' for yes and 'n' for no")
+        
+    else:
+        print("Error, please enter encrypt or decrypt to start: ")
