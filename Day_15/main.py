47139fcd2bbdf6e1bb14d881f6aca64444596c63
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-02-18 22:12:49 +0200

    Coffee with OOP

--- Day_15/main.py
+++ Day_15/main.py
@@ -2,8 +2,24 @@ from coffee_maker import *
 from menu import *
 from money_machine import *
 
-# printing the report 
-report = CoffeeMaker()
+
+coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
-report.report()
-money_machine.report()
+menu = Menu()
+
+
+is_on = True
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? {options} ? : ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
