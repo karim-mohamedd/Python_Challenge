c78df5ba0361e250cc215fb81c564f6a234d92a1
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-02-18 21:09:13 +0200

    Coffee Machine with OOP

---
+++ Day_15/main.py
@@ -0,0 +1,9 @@
+from coffee_maker import *
+from menu import *
+from money_machine import *
+
+# printing the report 
+report = CoffeeMaker()
+money_machine = MoneyMachine()
+report.report()
+money_machine.report()
