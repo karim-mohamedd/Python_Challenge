c94229e635ba5faa14182ee11f8fe173cd90f115
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-02-19 22:07:01 +0200

    main

--- Day_16/main.py
+++ Day_16/main.py
@@ -1,6 +1,8 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
+#Question Bank is a list of objects
 Question_Bank = []
 for question in question_data:
     q_text = question["text"]
