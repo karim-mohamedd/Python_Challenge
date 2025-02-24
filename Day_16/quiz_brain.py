08dd281c9cb9605233baab144844c0e444eb4b18
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-02-24 13:43:06 +0200

    Final Project

--- Day_16/quiz_brain.py
+++ Day_16/quiz_brain.py
@@ -1,7 +1,28 @@
 class QuizBrain:
-    def __init__(self, q_list):
-        self.question_num = 0
+
+    def __init__(self, q_list):  # constructor
+        self.question_number = 0
+        self.score = 0
         self.question_list = q_list
-    def nex_question(self, ):
-        current_question = self.question_list[self.question_num]
-        self.question_num = 1
+
+# Method are the action which objects do
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)  # checks if the quiz completes or not
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]  # get hold of the question in the list
+        self.question_number += 1
+        user_answer = input(f"Q{self.question_number}: {current_question.text}. (True/False)?: ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got right!")
+            self.score += 1
+        else:
+            print("That's Wrong!")
+        print(f"The current answer was: {correct_answer}.")
+        print(f"Your current score is {self.score}/{self.question_number}")
+        print("\n")
+
+
