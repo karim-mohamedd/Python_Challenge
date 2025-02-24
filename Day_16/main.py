993222ece6799a885f91cc29eaf23b7a1282ebb5
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-02-24 13:42:33 +0200

    main

--- Day_16/main.py
+++ Day_16/main.py
@@ -9,3 +9,14 @@ for question in question_data:
     a_answer = question["answer"]
     new_question = Question(q_text, a_answer)
     Question_Bank.append(new_question)
+
+quiz = QuizBrain(Question_Bank)   
+
+while quiz.still_has_question():
+        quiz.next_question()
+
+print("You have completed the quiz")
+print(f"You total score is {quiz.score}/{quiz.question_number}")
+
+
+
