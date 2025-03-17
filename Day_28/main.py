3b8052964dc43f709b7937fb9eb3e5249a64ed62
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-03-17 17:06:05 +0200

    Real Pomodoro timer for studying

---
+++ Day_28/main.py
@@ -0,0 +1,54 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+CHECK_MARK = "✔"
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5*60)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")                #it is how u change item in canvas class
+    if count > 0:
+        window.after(1000, count_down, count - 1)  # This is very important because it waits for specific time in ms and do some function
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW )
+           
+#----------------------------------------------------------
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+check_mark = Label(text=CHECK_MARK, fg=GREEN, bg=YELLOW)
+check_mark.grid(column=1, row=3)
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+#-----------------------------------------------------------
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)  #highlightthickness is to remove the border of the photo #canvas is class allows u to add layers on each other like text on image etc
+tomato_img = PhotoImage(file="tomato.png")  # This is the way to get hold of photo and use it 
+canvas.create_image(100, 112, image = tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white",font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+
+
+
+window.mainloop()
