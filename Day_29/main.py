d4b067274f22e70e4c160fbff1269550cef14251
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-03-20 17:22:24 +0200

    Local Password manager with tkinter

--- Day_29/main.py
+++ Day_29/main.py
@@ -4,7 +4,6 @@ import random
 
 EXPECTED_PASS = 8
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -20,8 +19,6 @@ def generate_password():
 
     password = "".join(password_list)
     password_entry.insert(0, password)
-    
-
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_password():
     website = website_entry.get()
@@ -43,12 +40,12 @@ def add_password():
     password_entry.delete(0, 'end')
     website_entry.focus()
 
-# ---------------------------- UI SETUP ------------------------------- #
+# ---------------------------- UI SETUP --------------------------------------- #
 window = Tk()
 window.title("password man")
 window.config(padx=20,pady=20)
 
-#--------------------------------- Labels --------------------------------------
+#--------------------------------- Labels ------------------------------------- #
 website_label = Label(text="Website/Username: ")
 website_label.grid(row=1, column=0, sticky="e")
 
@@ -58,7 +55,7 @@ email_label.grid(row=2, column=0, sticky="e")
 password_label = Label(text="Password: ")
 password_label.grid(row=3, column=0, sticky="e")
 
-#-------------------------------- Entries ---------------------------------------
+#-------------------------------- Entries ------------------------------------- #
 website_entry = Entry(width=39)
 website_entry.grid(row=1, column=1, columnspan=2, pady=5)
 website_entry.focus()
@@ -70,13 +67,13 @@ email_entry.insert(0, "karimelwaraky50@gmail.com")
 password_entry = Entry(width=26)
 password_entry.grid(row=3, column=1, pady=5, sticky="e")
 
-#------------------------------ Buttons -------------------------------------------
+#------------------------------ Buttons ---------------------------------------- #
 generate_password_button = Button(text="Generate Password", width=15, command=generate_password)
 generate_password_button.grid(row=3, column=2, padx=1, pady=3)
 
 add_password_button = Button(text="Add", width=33, command=add_password)
 add_password_button.grid(row=4, column=1, columnspan=2)
-#----------------------------------------------------------------------------------
+#------------------------------------------------------------------------------- #
 
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 password_man = PhotoImage(file="logo.png")
