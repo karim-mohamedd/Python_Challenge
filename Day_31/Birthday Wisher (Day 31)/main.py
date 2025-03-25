ab3257c980d4514e724b292791b9ebb5f68c09dc
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-03-25 02:00:33 +0200

    .\main.py

---
+++ Day_31/Birthday Wisher (Day 31)/main.py
@@ -0,0 +1,25 @@
+from datetime import *
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "karimelwaraky50@gmail.com"
+PASSWORD = " Your app password that is generated in gmail called 'app password' "
+today = (datetime.now().month, datetime.now().day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthday_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+
+if today in birthday_dict:
+    birthday_person = birthday_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}"
+    with open(file=file_path, mode="r") as letter_file:
+        content = letter_file.read()
+        content = content.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=birthday_person["email"], msg=f"Subject:Happy Birthday!\n\n{content}")
