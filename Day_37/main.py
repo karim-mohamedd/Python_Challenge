fcdadb53d041b8dd117c68ee0de2356bc9e71549
karim-mohamedd <karimmohamed5555555@gmail.com>
2025-04-20 21:44:37 +0200

    Trading project

--- Day_37/main.py
+++ Day_37/main.py
@@ -93,4 +93,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-
